31afa9bb5293f92ce10ccb50c7bedf2283baebc4
amoriqbal <43443370+amoriqbal@users.noreply.github.com>
2019-10-27 01:36:22 +0530

    2nd edit

--- index.php
+++ index.php
@@ -12,6 +12,15 @@
         <?php
         require 'Includes/header.php';
         ?>
+        <div class="container">
+            <div class="row" >
+                
+                <form action="Scripts/TakeTest.php" method="get">
+                    <input type="text" name="testname" value="no_test_selected" />
+                    <input type="button" value="Take a Test" action="submit" name="take_test" disabled=<?php if(isset($testname)==true && $testname=="no_test_selected") echo '"true"'; else echo '"false"' ;?>/>
+                </form>
+            </div>
+        </div>
         
     </body>
 </html>
