49313f3a541223bafa115968dfdc9112f40b8a8f
amoriqbal <43443370+amoriqbal@users.noreply.github.com>
2019-11-05 05:41:15 +0530

    post system last update

--- getQP.php
+++ getQP.php
@@ -1,6 +1,28 @@
 <?php
-$con=mysqli_connect("localhost","root","","qbank") or die("connection error");
-$query="SELECT * FROM questions;";
-$query_res= mysqli_query($con, $query);
-echo $_POST['diff']." ".$_POST['tim']." ".$_POST['sub'];
+
+$con=mysqli_connect("localhost","root","","qbank") ;
+$query="SELECT options,content FROM qstn;";
+$query_res=mysqli_query($con, $query) ;
+
+
+
+$i=1;
+class qpaper{
+    var $q;
+    var $num;
+}$toSend;
+$n=mysqli_num_rows($query_res);
+//echo '{"qpaper":[';
+//$qarray[]=array();
+while($i<=$n)
+{
+     //echo json_encode(mysqli_fetch_object($query_res));
+     $row= mysqli_fetch_assoc($query_res);
+     $qarray[]=$row;
+     $i++;
+}
+$toSend->q=$qarray;
+$toSend->num=$n;
+echo json_encode($toSend);
+$con->close();
 ?>
